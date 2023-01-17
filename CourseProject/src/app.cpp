ae04491e3c97e7cdfb0eee52010e7352665e2dbf
Kristian Dimitrov <krisdimitrov03@students.softuni.bg>
2023-01-18 00:09:11 +0200

    App Optimized

--- CourseProject/src/app.cpp
+++ CourseProject/src/app.cpp
@@ -7,7 +7,11 @@
 #include "../include/helper/size_constants.h"
 #include "../include/app.h"
 
-void runChoice(int choice, bool& exit);
+void runHashChoice(int choice, bool& exit);
+void runDehashChoice(int choice, bool& exit);
+void runSettingsChoice(int choice, bool& exit);
+void runExitChoice(int choice, bool& exit);
+void runErrorCase();
 
 void app::run() {
 	int choice;
@@ -16,64 +20,77 @@ void app::run() {
 		system("cls");
 		console::printMainMenu();
 		std::cin >> choice;
-
 		bool exit = false;
-		runChoice(choice, exit);
+
+		switch (choice) {
+		case 1:
+			runHashChoice(choice, exit);
+			break;
+		case 2:
+			runDehashChoice(choice, exit);
+			break;
+		case 3:
+			runSettingsChoice(choice, exit);
+			break;
+		case 4:
+			runExitChoice(choice, exit);
+			break;
+		default:
+			runErrorCase();
+			break;
+		}
 
 		if (exit)
 			return;
 	}
 }
 
-void runChoice(int choice, bool& exit) {
-	//hash
-	if (choice == 1) {
-		//printl(hash(TEST_INPUT));
+void runHashChoice(int choice, bool& exit) {
+	//printl(hash(TEST_INPUT));
 		//printl(hash("hello world"));
 		//system("cls");
-		console::printl("Make sure the text in your file is no longer than 500 characters.");
-		console::print("Enter file name: ");
-		char* filename = new char[MAX_MESSAGE_LENGTH + 2]{ '0' };
-		std::cin >> filename;
-		bool nameExists = false;
-		const char* message = file::read(filename, nameExists);
+	console::printl("Make sure the text in your file is no longer than 500 characters.");
+	console::print("Enter file name: ");
+	char* filename = new char[MAX_MESSAGE_LENGTH + 2]{ '0' };
+	std::cin >> filename;
+	bool nameExists = false;
+	const char* message = file::read(filename, nameExists);
 
-		if (!nameExists) {
-			console::print(" Press any key to continue...");
-			(void)_getch();
-			return;
-		}
-
-		const char* result = sha256::hash(TEST_INPUT);
-		console::print("Your hashed message: ");
-		console::printl(result);
-		console::printl("Press any key to continue...");
+	if (!nameExists) {
+		console::print(" Press any key to continue...");
 		(void)_getch();
-	}
-	//dehash
-	else if (choice == 2) {
-
-	}
-	//settings
-	else if (choice == 3) {
-		system("cls");
-		choice = 0;
-		console::printSettingsMenu();
-		std::cin >> choice;
-		//cases of settings
-	}
-	//exit
-	else if (choice == 4) {
-		exit = true;
-		system("cls");
-		console::printl("Bye, bye, User! :(");
 		return;
 	}
-	//error
-	else {
-		system("cls");
-		console::printErrorMessage(WRONG_INPUT_MESSAGE);
-		console::printl("\n");
-		_getch();
-	}
+
+	const char* result = sha256::hash(TEST_INPUT);
+	console::print("Your hashed message: ");
+	console::printl(result);
+	console::printl("Press any key to continue...");
+	(void)_getch();
+}
+
+void runDehashChoice(int choice, bool& exit) {
+
+}
+
+void runSettingsChoice(int choice, bool& exit) {
+	system("cls");
+	choice = 0;
+	console::printSettingsMenu();
+	std::cin >> choice;
+	//cases of settings
+}
+
+void runExitChoice(int choice, bool& exit) {
+	exit = true;
+	system("cls");
+	console::printl("Bye, bye, User! :(");
+	return;
+}
+
+void runErrorCase() {
+	system("cls");
+	console::printErrorMessage(WRONG_INPUT_MESSAGE);
+	console::printl("\n");
+	(void)_getch();
 }
